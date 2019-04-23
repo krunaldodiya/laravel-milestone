ea1f4a78d09f5fc06f647e02d85f11c1649735ec
krunal dodiya <kunal.dodiya1@gmail.com>
2019-04-23 21:54:46 +0530

    init

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ Route::group(['prefix' => 'device', 'middleware' => 'guest:api'], function () {
 
 Route::group(['prefix' => 'users', 'middleware' => 'auth:api'], function () {
     Route::post('/me', 'UserController@me');
+    Route::post('/update', 'UserController@update');
 });
 
 Route::group(['prefix' => 'home', 'middleware' => 'auth:api'], function () {
